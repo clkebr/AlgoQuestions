e4e644dbe3e5e77cf668787ed30171c10e5a4113
ebr <clkebr2601@gmail.com>
2023-01-31 19:02:58 +0100

    first repeating char

---
+++ src/main/java/org/algo/FirstRepeatedChar.java
@@ -0,0 +1,25 @@
+package org.algo;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class FirstRepeatedChar {
+    /*return the first repeated char in a string for example "a green apple" the first repeated char is e */
+    public static void main(String[] args) {
+        System.out.println("firstRepeatedChar(\"a green apple\") = " + firstRepeatedChar("a green apple"));
+    }
+
+    public static char firstRepeatedChar(String str) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i <str.length(); i++) {
+            if(set.contains(str.charAt(i))){
+                return str.charAt(i);
+            }else{
+               set.add(str.charAt(i));
+            }
+        }
+        return Character.MIN_VALUE;
+    }
+}
