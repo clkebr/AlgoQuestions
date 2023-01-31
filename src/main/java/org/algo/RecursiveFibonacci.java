8a150182dc94851d6e8d43f84ab0d0d4035db686
ebr <clkebr2601@gmail.com>
2023-01-31 15:33:40 +0100

    RecursiveFibonacci

--- src/main/java/org/algo/RecursiveFibonacci.java
+++ src/main/java/org/algo/RecursiveFibonacci.java
@@ -7,13 +7,13 @@ public class RecursiveFibonacci {
 
     public static void main(String[] args) {
 
-        System.out.println(fib(3));
+        System.out.println(fib(6));
     }
     //1st way
     public static int fib(int n){
         if(n==0) return 0;
         if(n==1) return 1;
-        else  return n*fib(n-1);
+        else  return fib(n-1)+fib(n-2);
 
     }
 
